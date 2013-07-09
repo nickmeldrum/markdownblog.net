afa7807702157b296db650426b6fd94a4741969f
Nick Meldrum <nick@nickmeldrum.com>
2013-07-09 22:37:06 +0100

    turned twitter "aside" helper into a partial view based on twitter settings in sitesettings.config

--- Web/Models/Site.cs
+++ Web/Models/Site.cs
@@ -9,13 +9,23 @@ namespace MarkdownBlog.Net.Web.Models {
         public string Owner { get { return ConfigurationManager.AppSettings["Owner"] ?? "Default Site Owner Name"; } }
 
         public string DisqusShortName { get { return ConfigurationManager.AppSettings["DisqusShortName"] ?? "DefaultDisqusShortName"; } }
-
+        
         public StackOverflowFlair StackOverflowFlair {
             get {
                 return new StackOverflowFlair {
                     Id = int.Parse(ConfigurationManager.AppSettings["StackOverflowFlairId"] ?? "-1"),
                     UserName = ConfigurationManager.AppSettings["StackOverflowFlairUserName"] ?? "DefaultStackOverflowUserName",
                     DisplayName = ConfigurationManager.AppSettings["StackOverflowFlairDisplayName"] ?? "Default Stack Overflow Display Name"
+                };
+            }
+        }
+
+        public TwitterTimeline TwitterTimeline {
+            get {
+                return new TwitterTimeline {
+                    Query = ConfigurationManager.AppSettings["TwitterTimelineQuery"] ?? "@DefaultTwitterQuery",
+                    Url = ConfigurationManager.AppSettings["TwitterTimelineUrl"] ?? "https://twitter.com/DefaultTwitterTimelineUrl",
+                    WidgetId = long.Parse(ConfigurationManager.AppSettings["TwitterTimelineWidgetId"] ?? "-1")
                 };
             }
         }
--- docs/todo.md
+++ docs/todo.md
@@ -1,11 +1,10 @@
 TODO:
 =====
 
-  * add Sitesettings.config for all the blog owner settings - and load into Site.cs
   * microformats?
   * add blog config for user specific settings
   * add gravatar plugin + author email in metadata
-  * make twitter plugin work off site settings - + twitter plugin for my latest tweets
+  * twitter plugin for my latest tweets
   * add some nicer styling - bootstrap theme?
   * fix up bug where disqus comments go below floated elements
   * add sitemap + ga
